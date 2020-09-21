079a65a0595d4ebc755ef448845bb52ef65d8734
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-09-21 13:12:22 +0300

    done2

--- New folder/script.js
+++ New folder/script.js
@@ -1,15 +1,43 @@
-function insert(num){
- calculator.display.value = calculator.display.value + num; 
+
+
+var zerobtn = document.getElementById("zerobtn");
+var onebtn = document.getElementById("onebtn");
+var twobtn = document.getElementById("twobtn");
+var threbtn = document.getElementById("threbtn");
+var fourbtn = document.getElementById("fourbtn");
+var fivbtn = document.getElementById("fivbtn");
+var sixbtn = document.getElementById("sixbtn");
+var sevenbtn = document.getElementById("sevenbtn");
+var eightbtn = document.getElementById("eightbtn");
+var ninebtn = document.getElementById("ninebtn");
+var dotbtn = document.getElementById("decimal");
+var cbtn = document.getElementById("clear")
+
+var numbtn = document.getElementsByClassName("btn-num");
+var operatorbtn = document.getElementsByClassName("btn-operator");
+
+
+function insert(val){
+ calculator.display.value = calculator.display.value + val ; 
 }
 
-// result of operations
-function equal(){
-  var result = calculator.display.value;
-  calculator.display.value = eval(result);
-}
+function equal() { 
 
+if (operatorbtn.value === "+") {  
+result += calculator.numbtn.value ;  
+
+} else if (operatorbtn.value === "*") {  
+result *= calculator.numbtn.value;  
+  
+} else if (operatorbtn.value === "-") {  
+result -= calculator.numbtn.value  
+  
+} else if (operatorbtn.value === "/") {   
+result /= calculator.numbtn.value; 
+}
+}
 
 // clearing text-box 
 function remove(){
- calculator.display.value = ""; 
+calculator.display.value = ""; 
 }
